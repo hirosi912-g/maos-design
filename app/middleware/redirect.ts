af888a9b9d061cc8e08f6c0330c7c6eb160f704b
chasel.liu <chasel.liu@fetchtw.com>
2026-03-30 15:16:44 +0800

    test

--- app/middleware/redirect.ts
+++
@@ -1,8 +0,0 @@
-const appStore = useAppStore();
-
-export default function ({ redirect }: any) {
-  if (appStore.firstPage) {
-    // return navigateTo('/' + appStore.firstPage);
-    return redirect('/' + appStore.firstPage);
-  }
-}
